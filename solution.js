f371caa6f657f366abf37467c96f4c44ed24bb59
Hanifa Arrumaisha <hanifa.aisha@gmail.com>
2020-05-30 00:59:59 +0700

    add: get linkpages and get link promos, but still not desired format

--- solution.js
+++ solution.js
@@ -27,6 +27,22 @@ let linkCreditCard = '';
 //         'totalPages' : 0
 //     ]
 
+/* how to change from 
+[
+    [sub
+        [page
+            {detail},
+            {detail}
+        ],
+    ],
+]
+to 
+[
+    title category: [detail],
+
+]
+*/
+
 // return list of categories,page
 var getPage =  function (url) {
     // var url = `${baseUrl}?product=0&subcat=${subcategory}&page=${page}`;
@@ -48,11 +64,18 @@ var getPage =  function (url) {
 };
 
 
-var writeResult = ($) => {
+var writePage = ($) => {
     fs.writeFileSync("./promospage.html", $.html());
     return $;
     // return [{subcategory, page}] categories
 }
+
+var writeResult = (json) => {
+    fs.appendFileSync("./solution.json", JSON.stringify(json));
+    return json;
+    // return [{subcategory, page}] categories
+}
+
 
 var getLinkCreditCard = ($) => {
     linkCreditCard = getLinkFromClickAction($, 'kartukredit');
@@ -94,40 +117,68 @@ getTotalPage = ($) => {
     titleOnPage = $(".page_promo_lain")[1].attribs.title;
     pattern = /of ([0-9]+)/
     totalPage = titleOnPage.match(pattern)[1];
+    console.log("sini, ", totalPage);
     return totalPage;
 }
 
 var getLinkPages = (linkSubcategories) => {
-        return Promise.map(linkSubcategories, function(val){
-            console.log(val.link)
-            return getPage(val.link).then(getTotalPage).then((totalPage)=>{        
-                return {
-                    title: val.title,
-                    link: val.link,
-                    totalPage: totalPage
-                }
-            }).catch(function(reason) {
-                console.log('Handle rejected promise ('+reason+') here.');
-            })
+    return Promise.map(linkSubcategories, function (subcategory){
+        return getPage(subcategory.link).then(getTotalPage).then(
+            function (totalPages){
+                    linkPromos = _.range(totalPage);
+                    console.log("sono, ", totalPage);
+                    return Promise.map(linkPromos, function (linkPromo, idx){
+                        page = idx+1;
+                        return getPage(subcategory.link+"&page="+page).then(getLinkPromo);
+                    })
+                    }
+        )
         })
+        
+
+    
+        // return Promise.map(linkSubcategories, function(val){
+        //     console.log(val.link)
+        //     return getPage(val.link).then(getTotalPage).then((totalPage)=>{        
+        //         return {
+        //             title: val.title,
+        //             link: val.link,
+        //             totalPage: totalPage
+        //         }
+        //     }).catch(function(reason) {
+        //         console.log('Handle rejected promise ('+reason+') here.');
+        //     })
+        // })
 }
 
 
-var getLinkPromos = (linkSubcategories) => {
-    return Promise.map(linkSubcategories, function (sub) {
-        tmp = _.range(sub.totalPage);
-        return Promise.map(tmp, function (idx){
-            idx +=1;
-            getPage(sub.link+"&page="+idx).then(getLinkPromo).then(function (val){
-                return {
-                    title: sub.title,
-                    link: sub.link,
-                    totalPage: sub.totalPage,
-                    linkPromos : val
-                }
-            }).then(function (val){console.log(val)})
-        });
-})}
+var getLinkPromos = function (totalPages){
+    return Promise.map(totalPages, function (totalPage){
+        linkPromos = _.range(totalPage);
+        console.log(totalPage);
+        return Promise.map(linkPromos, function (linkPromo){
+            getPage(sub.link+"&page="+idx);
+            return getLinkPromo
+        })
+        }
+    )
+}
+
+// (linkSubcategories) => {
+//     return Promise.map(linkSubcategories, function (sub) {
+//         tmp = _.range(sub.totalPage);
+//         return Promise.map(tmp, function (idx){
+//             idx +=1;
+//             getPage(sub.link+"&page="+idx).then(getLinkPromo).then(function (val){
+//                 return {
+//                     title: sub.title,
+//                     link: sub.link,
+//                     totalPage: sub.totalPage,
+//                     linkPromos : val
+//                 }
+//             }).then(function (val){console.log(val)})
+//         });
+// })}
 
 var getLinkPromo = ($) => {
     result = [];
@@ -142,27 +193,40 @@ var getLinkPromo = ($) => {
 }
 
 getDetailPromo = (linkPromos) => {
-    return Promise.map(linkPromos, function (url){
-        getPage(url).then(function ($){
-            imageUrl = $(".keteranganinside img")[0].attribs.src;
-            period_class = $(".periode b").text();
-            console.log(period_class);
-            area = $(".periode b").text();
-
-            return {'imageUrl' : imageUrl,
-                'period_class': period_class, 
-                'area': area
+        Promise.map(linkPromos, function (category){
+            return Promise.map(category, function (page) {
+                return Promise.map(page, function (url){
+                    getPage(url).then(function ($){
+                        imageUrl = baseUrl+$(".keteranganinside img")[0].attribs.src;
+                        period_class = $(".periode b").text();
+                        console.log(period_class);
+                        area = $(".periode b").text();
+            
+                        result = {'imageUrl' : imageUrl,
+                            'period_class': period_class, 
+                            'area': area
+                        }
+                        console.log(result);
+                        return result;
+                })
+                })
             }
-    })}).catch(function(reason) {
+        ).catch(function(reason) {
         console.log('Handle rejected promise ('+reason+') here.');
     })
+})
 }
 
 // categories = [link to every categories]
-getPage(baseUrl).then(getLinkCreditCard).then(getPageCreditCard).then(writeResult).then(getLinkSubcategory)
+getPage(baseUrl)
+    .then(getLinkCreditCard)
+    .then(getPageCreditCard)
+    .then(getLinkSubcategory)
     .then(getLinkPages)
-    .then(getLinkPromos)
-    .then(function (val){console.log(val)})
+    .then(getDetailPromo)
+    .then(function (val){console.log("abis ");console.log(val);return val;})
+    .then(writeResult)
+    .then(function (val){console.log("abis ");console.log(val)})
     .catch(function(reason) {
         console.log('Handle rejected promise ('+reason+') here.');
     });
